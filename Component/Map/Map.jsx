3c0eb4118731b6949e83d22beda4f2250828dc7f
Iman <bajalaniman@gmail.com>
2025-08-21 10:26:18 +0200

    Adding research page to the map

--- Component/Map/Map.jsx
+++ Component/Map/Map.jsx
@@ -5,6 +5,7 @@ import ShowVideos from "./ShowVideos";
 import FilterYear from "./FilterYear";
 import SoilSamplesTable from "./Soil/SoilSamplesTable";
 import SoilInformationOnMap from "./Soil/SoilInformationOnMap";
+import Researches from "./Research/Researches";
 
 function Map() {
   const [plots, setPlots] = useState([]);
@@ -22,6 +23,7 @@ function Map() {
   const [soilLocations, setSoilLocations] = useState([]);
   const [showSoilData, setShowSoilData] = useState(false);
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [researchData, setResearchData] = useState(false);
 
   const [aerialImagesYear, setAerialImagesYear] = useState([
     { id: 0, year: "None" },
@@ -100,195 +102,227 @@ function Map() {
   };
 
   return (
-    <div className="lg:flex px-4 lg:px-0 lg:justify-start w-full ">
-      {zoomLevel > 14 ? (
-        <>
-          <div className="flex flex-col">
-            <div className="flex mb-4 gap-1">
-              {/* Species Button */}
-              <button
-                onClick={() => {
-                  setShowPanorama(false);
-                  setShowVideo(false);
-                  setTreeVideo({});
-                  setShowFilter(true);
-                  setShowSoilData(false);
-                }}
-                className={`w-[70px] py-2 rounded ${
-                  showFilter && (!treeVideo.tree_id || !showVideo)
-                    ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
-                    : "bg-slate-200 text-black shadow-lg border border-slate-400"
-                }`}
-              >
-                Species
-              </button>
-
-              {/* 360 Button */}
-              <button
-                onClick={() => {
-                  setShowFilter(false);
-                  setShowVideo(false);
-                  setTreeVideo({});
-                  setShowPanorama(true);
-                  setShowSoilData(false);
-                }}
-                className={`w-[70px] py-2 rounded ${
-                  showPanorama && (!treeVideo.tree_id || !showVideo)
-                    ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
-                    : "bg-slate-200 text-black shadow-lg border border-slate-400"
-                }`}
-              >
-                Image
-              </button>
-
-              <button
-                onClick={() => {
-                  setShowFilter(false);
-                  setShowVideo(false);
-                  setTreeVideo({});
-                  setShowPanorama(false);
-                  setShowSoilData(true);
-                }}
-                className={`w-[70px] py-2 rounded ${
-                  showSoilData && (!treeVideo.tree_id || !showVideo)
-                    ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
-                    : "bg-slate-200 text-black shadow-lg border border-slate-400"
-                }`}
-              >
-                Soil
-              </button>
-
-              {/* Video Button – only show if tree has video */}
-              {treeVideo?.tree_id && !showFilter && !showPanorama && (
+    <div className="flex flex-col">
+      <div className="lg:flex px-4 lg:px-0 lg:justify-start w-full">
+        {zoomLevel > 14 ? (
+          <>
+            <div className="flex flex-col">
+              <div className="flex mb-4 gap-1">
+                {/* Species Button */}
                 <button
                   onClick={() => {
-                    setShowFilter(false);
                     setShowPanorama(false);
-                    setShowVideo(true);
+                    setShowVideo(false);
+                    setTreeVideo({});
+                    setShowFilter(true);
                     setShowSoilData(false);
+                    setResearchData(false);
                   }}
                   className={`w-[70px] py-2 rounded ${
-                    showVideo
+                    showFilter && (!treeVideo.tree_id || !showVideo)
                       ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
                       : "bg-slate-200 text-black shadow-lg border border-slate-400"
                   }`}
                 >
-                  Video
+                  Species
                 </button>
-              )}
-            </div>
 
-            <hr className="w-[95%] lg:w-[80%]" />
+                {/* 360 Button */}
+                <button
+                  onClick={() => {
+                    setShowFilter(false);
+                    setShowVideo(false);
+                    setTreeVideo({});
+                    setShowPanorama(true);
+                    setShowSoilData(false);
+                    setResearchData(false);
+                  }}
+                  className={`w-[70px] py-2 rounded ${
+                    showPanorama && (!treeVideo.tree_id || !showVideo)
+                      ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
+                      : "bg-slate-200 text-black shadow-lg border border-slate-400"
+                  }`}
+                >
+                  Image
+                </button>
 
-            {/* Panel content */}
-            <div className="h-72 mb-3 lg:mb-0 lg:h-64 flex w-[400px]">
-              {showFilter && !treeVideo.tree_id && (
-                <div className="flex flex-col gap-4 w-96">
-                  {mapCenter && trees.some((t) => t.plot_id === mapCenter) ? (
-                    <>
-                      <FilterTrees
-                        trees={trees}
-                        selectedSpecies={selectedSpecies}
-                        setSelectedSpecies={setSelectedSpecies}
-                        speciesOptions={speciesOptions}
-                      />
-                      <hr className="w-[95%] lg:w-[80%]" />
-                      <FilterYear
-                        trees={trees}
-                        selectedYear={selectedYear}
-                        setSelectedSYear={setSelectedSYear}
-                        yearOptions={yearOptions}
-                      />
-                    </>
-                  ) : (
-                    <div className="w-[400px] lg:w-64 h-full flex justify-center items-center">
-                      <p>No data is available!!!</p>
-                    </div>
-                  )}
-                </div>
-              )}
+                <button
+                  onClick={() => {
+                    setShowFilter(false);
+                    setShowVideo(false);
+                    setTreeVideo({});
+                    setShowPanorama(false);
+                    setShowSoilData(true);
+                    setResearchData(false);
+                  }}
+                  className={`w-[70px] py-2 rounded ${
+                    showSoilData && (!treeVideo.tree_id || !showVideo)
+                      ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
+                      : "bg-slate-200 text-black shadow-lg border border-slate-400"
+                  }`}
+                >
+                  Soil
+                </button>
+                <button
+                  onClick={() => {
+                    setShowFilter(false);
+                    setShowVideo(false);
+                    setTreeVideo({});
+                    setShowPanorama(false);
+                    setShowSoilData(false);
+                    setResearchData(true);
+                  }}
+                  className={`w-[70px] py-2 rounded ${
+                    researchData && (!treeVideo.tree_id || !showVideo)
+                      ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
+                      : "bg-slate-200 text-black shadow-lg border border-slate-400"
+                  }`}
+                >
+                  Research
+                </button>
 
-              {zoomLevel > 14 && showSoilData && !treeVideo.tree_id && (
-                <SoilSamplesTable
-                  plotId={1}
-                  setSoilLocations={setSoilLocations}
-                  showSoilData={showSoilData}
-                />
-              )}
+                {/* Video Button – only show if tree has video */}
+                {treeVideo?.tree_id && !showFilter && !showPanorama && (
+                  <button
+                    onClick={() => {
+                      setShowFilter(false);
+                      setShowPanorama(false);
+                      setShowVideo(true);
+                      setShowSoilData(false);
+                      setResearchData(false);
+                    }}
+                    className={`w-[70px] py-2 rounded ${
+                      showVideo
+                        ? "bg-[#10bc98] text-white shadow-lg border border-slate-400"
+                        : "bg-slate-200 text-black shadow-lg border border-slate-400"
+                    }`}
+                  >
+                    Video
+                  </button>
+                )}
+              </div>
 
-              {showPanorama && !treeVideo.tree_id && (
-                <div className="flex gap-2 flex-col justify-center">
-                  {mapCenter && trees.some((t) => t.plot_id === mapCenter) ? (
-                    <div>
-                      {aerialImagesYear.map((i) => (
-                        <div className="w-[400px] lg:w-64 flex">
-                          <button
-                            className={`w-96 py-1 border 
+              <hr className="w-[95%] lg:w-[80%]" />
+
+              {/* Panel content */}
+              <div className="h-72 mb-3 lg:mb-0 lg:h-64 flex w-[400px]">
+                {showFilter && !treeVideo.tree_id && (
+                  <div className="flex flex-col gap-4 w-96">
+                    {mapCenter && trees.some((t) => t.plot_id === mapCenter) ? (
+                      <>
+                        <FilterTrees
+                          trees={trees}
+                          selectedSpecies={selectedSpecies}
+                          setSelectedSpecies={setSelectedSpecies}
+                          speciesOptions={speciesOptions}
+                        />
+                        <hr className="w-[95%] lg:w-[80%]" />
+                        <FilterYear
+                          trees={trees}
+                          selectedYear={selectedYear}
+                          setSelectedSYear={setSelectedSYear}
+                          yearOptions={yearOptions}
+                        />
+                      </>
+                    ) : (
+                      <div className="w-[400px] lg:w-64 h-full flex justify-center items-center">
+                        <p>No data is available!!!</p>
+                      </div>
+                    )}
+                  </div>
+                )}
+
+                {zoomLevel > 14 && showSoilData && !treeVideo.tree_id && (
+                  <SoilSamplesTable
+                    plotId={1}
+                    setSoilLocations={setSoilLocations}
+                    showSoilData={showSoilData}
+                  />
+                )}
+
+                {showPanorama && !treeVideo.tree_id && (
+                  <div className="flex gap-2 flex-col justify-center">
+                    {mapCenter && trees.some((t) => t.plot_id === mapCenter) ? (
+                      <div>
+                        {aerialImagesYear.map((i) => (
+                          <div className="w-[400px] lg:w-64 flex">
+                            <button
+                              className={`w-96 py-1 border 
                         ${
                           yearBtnColor === i.year
                             ? "bg-[#10bc98] text-white shadow-lg border border-slate-400 mt-3"
                             : "bg-slate-200 text-black shadow-lg border border-slate-400 mt-3"
                         } rounded-md`}
-                            key={i.id}
-                            onClick={() => aerialImagehandler({ year: i.year })}
-                          >
-                            {i.year}
-                          </button>
-                        </div>
-                      ))}
-                    </div>
-                  ) : (
-                    <div className="w-[400px] lg:w-64 h-full flex justify-center items-center">
-                      <p>"No image is available!!!"</p>
-                    </div>
-                  )}
-                </div>
-              )}
+                              key={i.id}
+                              onClick={() =>
+                                aerialImagehandler({ year: i.year })
+                              }
+                            >
+                              {i.year}
+                            </button>
+                          </div>
+                        ))}
+                      </div>
+                    ) : (
+                      <div className="w-[400px] lg:w-64 h-full flex justify-center items-center">
+                        <p>"No image is available!!!"</p>
+                      </div>
+                    )}
+                  </div>
+                )}
 
-              {treeVideo.tree_id && showVideo && (
-                <ShowVideos treeId={treeVideo.tree_id} />
-              )}
+                {treeVideo.tree_id && showVideo && (
+                  <ShowVideos treeId={treeVideo.tree_id} />
+                )}
+              </div>
             </div>
+          </>
+        ) : (
+          <div className="lg:pr-16 lg:w-[400px]">
+            <p className="text-justify py-2">
+              The primary objectives of this web tool are to centralize,
+              organize, and present data collected from various agroforestry
+              sites associated with Eberswalde University of Sustainable
+              Development. All relevant information is stored in a structured
+              database, allowing for efficient data management and
+              accessibility. You can interact with the map interface to explore
+              specific locations in greater detail. By zooming in and selecting
+              individual plots, additional data and functionalities become
+              available, offering a more comprehensive understanding of each
+              site.
+            </p>
           </div>
-        </>
-      ) : (
-        <div className="lg:pr-16 lg:w-[400px]">
-          <p className="text-justify py-2">
-            The primary objectives of this web tool are to centralize, organize,
-            and present data collected from various agroforestry sites
-            associated with Eberswalde University of Sustainable Development.
-            All relevant information is stored in a structured database,
-            allowing for efficient data management and accessibility. You can
-            interact with the map interface to explore specific locations in
-            greater detail. By zooming in and selecting individual plots,
-            additional data and functionalities become available, offering a
-            more comprehensive understanding of each site.
-          </p>
-        </div>
-      )}
+        )}
 
-      {/* Map container */}
-      <LeafletContainer
-        zoomLevel={zoomLevel}
-        setZoomLevel={setZoomLevel}
-        plots={plots}
-        filteredTrees={filteredTrees}
-        // filteredYears={filteredYears}
-        setTreeVideo={setTreeVideo}
-        setShowVideo={setShowVideo}
-        setShowFilter={setShowFilter}
-        setShowPanorama={setShowPanorama}
-        setMapCenter={setMapCenter}
-        aerialImagehandlerPass={aerialImagehandlerPass}
-        soilLocations={soilLocations}
-        showSoilData={showSoilData}
-        setSelectedLocation={setSelectedLocation}
-      />
-      {selectedLocation && (
-        <SoilInformationOnMap
-          location={selectedLocation}
-          onClose={() => setSelectedLocation(null)}
+        {/* Map container */}
+        <LeafletContainer
+          zoomLevel={zoomLevel}
+          setZoomLevel={setZoomLevel}
+          plots={plots}
+          filteredTrees={filteredTrees}
+          // filteredYears={filteredYears}
+          setTreeVideo={setTreeVideo}
+          setShowVideo={setShowVideo}
+          setShowFilter={setShowFilter}
+          setShowPanorama={setShowPanorama}
+          setMapCenter={setMapCenter}
+          aerialImagehandlerPass={aerialImagehandlerPass}
+          soilLocations={soilLocations}
+          showSoilData={showSoilData}
+          setSelectedLocation={setSelectedLocation}
         />
+        {selectedLocation && (
+          <SoilInformationOnMap
+            location={selectedLocation}
+            onClose={() => setSelectedLocation(null)}
+          />
+        )}
+      </div>
+
+      {zoomLevel > 14 && researchData && !treeVideo.tree_id && (
+        <div className="mb-24 h-[100%]">
+          <Researches />
+        </div>
       )}
     </div>
   );
